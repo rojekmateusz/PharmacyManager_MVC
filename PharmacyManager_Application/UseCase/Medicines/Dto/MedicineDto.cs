33a5bc25661dc951257cf6955ef7024a685a8996
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-03-31 14:25:11 +0200

    add medicine dto

---
+++ PharmacyManager_Application/UseCase/Medicines/Dto/MedicineDto.cs
@@ -0,0 +1,15 @@
+﻿using PharmacyManager_Domain.Entities;
+
+namespace PharmacyManager_Application.UseCase.Medicines.Dto;
+
+public class MedicineDto
+{
+    public int MedicineId { get; set; }
+    public string Name { get; set; } = default!;
+    public string Manufacturer { get; set; } = default!;
+    public decimal Price { get; set; }
+    public bool RequiresPrescription { get; set; }
+    public DateTime ExpiryDate { get; set; }
+    public int StockQuantity { get; set; }
+
+ }
