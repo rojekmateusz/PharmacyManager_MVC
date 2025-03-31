ff555768024ebc88d30d3d4b977572e7121abdaa
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-03-31 15:06:15 +0200

    add medicine profile; install mediatr, fluent validation and automapper to application layer

---
+++ PharmacyManager_Application/UseCase/Medicines/Dto/MedicineProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using PharmacyManager_Domain.Entities;
+
+namespace PharmacyManager_Application.UseCase.Medicines.Dto;
+
+public class MedicineProfile : Profile
+{
+    public MedicineProfile()
+    {
+        CreateMap<Medicine, MedicineDto>();
+    }
+}
